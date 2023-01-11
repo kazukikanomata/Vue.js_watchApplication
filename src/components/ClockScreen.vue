2dad93a64760aacd4b1a86a6b8b83d6211309183
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2023-01-11 19:03:56 +0900

    add：vueのコンポーネントを作成

---
+++ src/components/ClockScreen.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="container">
+        <div class="clock">
+            <div class="date">
+                <p>{{ year }}/ {{ month }}/{{ day }}</p>
+            </div>
+            <div class="time">
+                <p>{{ hours }}:{{ minutes }}<span class="second">:{{ seconds }}</span></p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "ClockScreen",
+        // dataはデータを定義する場所
+        data() {
+            return{
+                // 現在の日時を保持する。
+                date: new Date(),
+            };
+        },
+        // 保持したデータを加工する役割
+        // 画面の情報を更新するたびに自動で取得される
+        computed: {
+            // 年
+            year() {
+                return this.date.getFullYear();
+            },
+            // 月
+            // 0月から11月から取ってくる
+            month(){
+                return this.date.getMonth()+1;
+            },
+            // 日
+            day(){
+                return this.dateTimePadding(this.date.getDate());
+            },
+            // 時間
+            hours(){
+                return this.dateTimePadding(this.date.getHours());
+            },
+            // 分
+            minutes(){
+                return this.dateTimePadding(this.date.getMinutes());
+            },
+            // 秒
+            seconds(){
+                return this.dateTimePadding(this.date.getSeconds());   
+            }
+        },
+        mounted(){
+            //画面に表示される時に一番最初に読み込まれる。
+            //ライフサイクル
+            this.setDate();
+            // １秒ごとにsetDate()を実行する
+            setInterval(()=> this.setDate(), 1000);
+        },
+        methods: {
+            // 日時を二桁に
+            dateTimePadding(num){
+                return ("0" + num ).slice(-2);
+            },
+            // 現在日時をセット
+            setDate(){
+                this.date = new Date();
+            },
+        },
+    }
+
+
+
+</script>
+
+<style>
+.date p{
+    color: white;
+}
+.time p{
+    color: white;
+}
+</style>
